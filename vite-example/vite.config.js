e64611cc4a2816afc6c37b1f4d2c5e4cf3085b87
Walter Perdan <github@kalwaltart.it>
2025-11-27 23:17:57 +0100

    Update dependencies and add vite-plugin-static-copy to handle ARToolKit assets

--- vite-example/vite.config.js
+++ vite-example/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   optimizeDeps: {
@@ -10,4 +11,15 @@ export default defineConfig({
       allow: ['..'],
     },
   },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          // copy built ARToolKit JS files into Vite's served /assets
+          src: 'node_modules/@ar-js-org/arjs-plugin-artoolkit/dist/assets/ARToolkit-*.js',
+          dest: 'assets'
+        }
+      ]
+    })
+  ]
 });
